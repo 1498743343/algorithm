e2ca29923bd467b98e4444047f81d0ea9d8dc6e1
chenzihan <1498743343@qq.com>
2022-05-05 23:56:45 +0800

    feat: 提交二进制码和阶乘相关代码

--- src/main/java/com/chen/algorithm/Code01PrintBinary.java
+++ src/main/java/com/chen/algorithm/Code01PrintBinary.java
@@ -2,21 +2,29 @@ package com.chen.algorithm;
 
 /**
  * code01打印二进制
- *
+ * 对一个数做 - 操作，如：5-> -5，其实是对二进制码进行 ~ + 1
  * @author chenzihan
  * @date 2022/05/05
  */
-public class Code01_PrintBinary {
+public class Code01PrintBinary {
+    /**
+     * 打印一个int值的二进制码
+     *
+     * @param num 全国矿工工会
+     */
     public static void print(int num) {
         for (int i = 31; i >= 0; i--) {
             System.out.print((num & (1 << i)) == 0 ? "0" : 1);
         }
+        System.out.println();
     }
     public static void main(String[] args) {
-        int a = Integer.MIN_VALUE;
+        int a = 123;
         System.out.println(a);
         print(a);
         System.out.println(-a);
         print(-a);
+        System.out.println(~a +1 );
+        print(~a + 1);
     }
 }
--- src/main/java/com/chen/algorithm/Code02SumOfFactorial.java
+++ src/main/java/com/chen/algorithm/Code02SumOfFactorial.java
@@ -1,2 +1,51 @@
-package com.chen.algorithm;public class Code02SumOfFactorial {
+package com.chen.algorithm;
+
+/**
+ * code02求 1~n 的阶乘相加：1! + 2! + 3! + ... + n!
+ *
+ * @author chenzihan
+ * @date 2022/05/05
+ */
+public class Code02SumOfFactorial {
+    /**
+     * f1：每次都对所有的数进行阶乘操作，然后相加
+     * 性能低，重复步骤多
+     *
+     * @param n n
+     * @return long
+     */
+    public static long f1(int n) {
+        long result = 0;
+        for (int i = 1; i <= n; i++) {
+            long s = 1;
+            for (int j = 1; j <= i; j++) {
+                s *= j;
+            }
+            result += s;
+        }
+        return result;
+    }
+
+    /**
+     * f2：每次记录上次阶乘的结果，然后 *n 后加到 result 中
+     * 性能高，避免了之前的重复阶乘
+     *
+     * @param n n
+     * @return long
+     */
+    public static long f2(int n) {
+        long result = 0;
+        int s = 1;
+        for (int i = 1; i <= n; i++) {
+            s *= i;
+            result += s;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        long result = f2(n);
+        System.out.println("result = " + result);
+    }
 }
