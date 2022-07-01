143197e676599f013d92e246604618ab71c614da
chenzihan <chenzihan@thunisoft.com>
2022-07-01 17:10:19 +0800

    feat: 完善堆排，新增随机快排

--- src/main/java/com/chen/algorithm/day15/review/QuickSort.java
+++ src/main/java/com/chen/algorithm/day15/review/QuickSort.java
@@ -3,12 +3,12 @@ package com.chen.algorithm.day15.review;
 import java.util.Arrays;
 
 /**
- * 堆排序
+ * 快速排序
  *
  * @author chenzihan
- * @date 2022/06/30
+ * @date 2022/07/01
  */
-public class HeapSort {
+public class QuickSort {
     public static void main(String[] args) {
         int maxLength = 1000;
         int maxValue = 10000;
@@ -18,7 +18,7 @@ public class HeapSort {
             int[] array1 = genernateRandomArray(maxLength, maxValue);
             int[] array2 = Arrays.copyOf(array1, array1.length);
             int[] array3 = Arrays.copyOf(array1, array1.length);
-            heapSort(array1);
+            quickSort(array1);
             Arrays.sort(array2);
             if (!Arrays.equals(array1, array2)) {
                 print(array3);
@@ -29,40 +29,38 @@ public class HeapSort {
         System.out.println("测试结束");
     }
 
-    private static void print(int[] array3) {
-        for (int j : array3) {
-            System.out.print(j + " ");
-        }
-    }
-
-    private static void heapSort(int[] array) {
+    private static void quickSort(int[] array) {
         if (array == null || array.length <= 1) {
             return;
         }
-        int length = array.length;
-        for (int i = length - 1; i >= 0; i--) {
-            heapify(array, i, length);
-        }
-        while (length > 1) {
-            swap(array, 0, --length);
-            heapify(array, 0, length);
-        }
+        process(array, 0, array.length - 1);
     }
 
-    private static void heapify(int[] array, int index, int heapSize) {
-        int left = 2 * index + 1;
-        while (left < heapSize) {
-            int largest = left;
-            if (left + 1 < heapSize && array[left + 1] > array[left]) {
-                largest = left + 1;
-            }
-            if (array[index] >= array[largest]) {
-                return;
-            }
-            swap(array, index, largest);
-            index = largest;
-            left = 2 * index + 1;
+    private static void process(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
         }
+        int[] range = partation(array, left, right);
+        process(array, left, range[0]);
+        process(array, range[1], right);
+    }
+
+    private static int[] partation(int[] array, int left, int right) {
+        int index = left + (int) (Math.random() * (right - left + 1));
+        int targetNumber = array[index];
+        int leftBorder = left - 1;
+        int rightBorder = right + 1;
+        index = left;
+        while (index < rightBorder) {
+            if (array[index] < targetNumber) {
+                swap(array, index++, ++leftBorder);
+            } else if (array[index] == targetNumber) {
+                index++;
+            } else {
+                swap(array, index, --rightBorder);
+            }
+        }
+        return new int[]{leftBorder, rightBorder};
     }
 
     private static int[] genernateRandomArray(int maxLength, int maxValue) {
@@ -78,5 +76,11 @@ public class HeapSort {
         int tmp = array[i];
         array[i] = array[j];
         array[j] = tmp;
+    }
+
+    private static void print(int[] array3) {
+        for (int j : array3) {
+            System.out.print(j + " ");
+        }
     }
 }
