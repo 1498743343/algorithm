25379bcddfcf8c82f9327dd0fc22ecf65e29adee
chenzihan <chenzihan@thunisoft.com>
2022-07-04 14:03:34 +0800

    feat: 快慢指针寻找链表节点

---
+++ src/main/java/com/chen/algorithm/day15/linkedlist/FastPointerAndSlowPointer.java
@@ -0,0 +1,178 @@
+package com.chen.algorithm.day15.linkedlist;
+
+import com.chen.algorithm.day4.Node;
+
+import java.util.ArrayList;
+
+/**
+ * 快慢指针
+ * 快指针一次走两个节点，慢指针一次走一个
+ * 使用快慢指针遍历链表，一次遍历将链表的中点和尾部节点同时拿到，在一些面试题中非常常用
+ *
+ * @author chenzihan
+ * @date 2022/07/01
+ */
+public class FastPointerAndSlowPointer {
+    /**
+     * 输入链表头节点，奇数长度返回中点，偶数长度返回上中点
+     *
+     * @param head 头
+     */
+    public static Node m1(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node fastPointer = head.next.next;
+        Node slowPointer = head.next;
+        while (fastPointer.next != null && fastPointer.next.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    /**
+     * 输入链表头节点，奇数长度返回中点，偶数长度返回下中点
+     *
+     * @param head 头
+     * @return {@link Node}
+     */
+    public static Node m2(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node fastPointer = head.next;
+        Node slowPointer = head.next;
+        while (fastPointer.next != null && fastPointer.next.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    /**
+     * 输入链表头节点，奇数长度返回中点前一个，偶数长度返回上中点前一个
+     *
+     * @param head 头
+     * @return {@link Node}
+     */
+    public static Node m3(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node fastPointer = head.next.next;
+        Node slowPointer = head;
+        while (fastPointer.next != null && fastPointer.next.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+        return slowPointer;
+    }
+
+    /**
+     * 输入链表头节点，奇数长度返回中点前一个，偶数长度返回下中点前一个
+     *
+     * @param head 头
+     * @return {@link Node}
+     */
+    public static Node m4(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node fastPointer = head.next;
+        Node slowPointer = head;
+        while (fastPointer.next != null && fastPointer.next.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        return slowPointer;
+    }
+
+    public static void main(String[] args) {
+        Node test = null;
+        test = new Node(0);
+        test.next = new Node(1);
+        test.next.next = new Node(2);
+        test.next.next.next = new Node(3);
+        test.next.next.next.next = new Node(4);
+        test.next.next.next.next.next = new Node(5);
+        test.next.next.next.next.next.next = new Node(6);
+        test.next.next.next.next.next.next.next = new Node(7);
+        test.next.next.next.next.next.next.next.next = new Node(8);
+
+        Node ans1;
+        Node ans2;
+
+        ans1 = m1(test);
+        ans2 = right1(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
+
+        ans1 = m2(test);
+        ans2 = right2(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
+
+        ans1 = m3(test);
+        ans2 = right3(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
+
+        ans1 = m4(test);
+        ans2 = right4(test);
+        System.out.println(ans1 != null ? ans1.value : "无");
+        System.out.println(ans2 != null ? ans2.value : "无");
+    }
+
+    public static Node right1(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get((arr.size() - 1) / 2);
+    }
+
+    public static Node right2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get(arr.size() / 2);
+    }
+
+    public static Node right3(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get((arr.size() - 3) / 2);
+    }
+
+    public static Node right4(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node cur = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        while (cur != null) {
+            arr.add(cur);
+            cur = cur.next;
+        }
+        return arr.get((arr.size() - 2) / 2);
+    }
+}
