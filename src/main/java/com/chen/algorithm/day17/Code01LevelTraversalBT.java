4595d16c9bb7c246cadc1fce7c3338e809152e2a
chenzihan <1498743343@qq.com>
2022-07-18 22:37:52 +0800

    feat: 按层遍历二叉树

---
+++ src/main/java/com/chen/algorithm/day17/Code01LevelTraversalBT.java
@@ -0,0 +1,56 @@
+package com.chen.algorithm.day17;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * code01 按层遍历二叉树
+ *
+ * @author chenzihan
+ * @date 2022/07/18
+ */
+public class Code01LevelTraversalBT {
+
+	public static class Node {
+		public int value;
+		public Node left;
+		public Node right;
+
+		public Node(int v) {
+			value = v;
+		}
+	}
+
+	public static void level(Node head) {
+		if (head == null) {
+			return;
+		}
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(head);
+		while (!queue.isEmpty()) {
+			Node poll = queue.poll();
+			System.out.print(poll.value + " ");
+			if (poll.left != null) {
+				queue.add(poll.left);
+			}
+			if (poll.right != null) {
+				queue.add(poll.right);
+			}
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.left = new Node(2);
+		head.right = new Node(3);
+		head.left.left = new Node(4);
+		head.left.right = new Node(5);
+		head.right.left = new Node(6);
+		head.right.right = new Node(7);
+
+		level(head);
+		System.out.println("========");
+	}
+
+}
