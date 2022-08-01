7d87e8fb7fe52aaa2adc160eb43c3578622391fb
chenzihan <chenzihan@thunisoft.com>
2022-08-01 16:12:18 +0800

    feat: 根据给定的数据结构，求当前节点的后继节点

---
+++ src/main/java/com/chen/algorithm/day17/Code06SuccessorNode.java
@@ -0,0 +1,96 @@
+package com.chen.algorithm.day17;
+
+/**
+ * code06后继节点
+ * 根据给定的数据结构，求当前节点的后继节点
+ *
+ * @author chenzihan
+ * @date 2022/08/01
+ */
+public class Code06SuccessorNode {
+
+	public static class Node {
+		public int value;
+		public Node left;
+		public Node right;
+		public Node parent;
+
+		public Node(int data) {
+			this.value = data;
+		}
+	}
+
+	public static Node getSuccessorNode(Node node) {
+		if (node == null) {
+			return null;
+		}
+		// 当前节点有右子树，右孩子的最左侧节点就是后继节点
+		if (node.right != null) {
+			return getLeftMost(node.right);
+		} else {
+			// 当前节点没有右孩子，往上看，当当前节点是父节点的左孩子或者父节点是头节点时，返回父节点
+			Node parent = node.parent;
+			while (parent != null && node == parent.right) {
+				node = parent;
+				parent = parent.parent;
+			}
+			return parent;
+		}
+	}
+
+	public static Node getLeftMost(Node node) {
+		if (node == null) {
+			return null;
+		}
+		while (node.left != null) {
+			node = node.left;
+		}
+		return node;
+	}
+
+	public static void main(String[] args) {
+		Node head = new Node(6);
+		head.parent = null;
+		head.left = new Node(3);
+		head.left.parent = head;
+		head.left.left = new Node(1);
+		head.left.left.parent = head.left;
+		head.left.left.right = new Node(2);
+		head.left.left.right.parent = head.left.left;
+		head.left.right = new Node(4);
+		head.left.right.parent = head.left;
+		head.left.right.right = new Node(5);
+		head.left.right.right.parent = head.left.right;
+		head.right = new Node(9);
+		head.right.parent = head;
+		head.right.left = new Node(8);
+		head.right.left.parent = head.right;
+		head.right.left.left = new Node(7);
+		head.right.left.left.parent = head.right.left;
+		head.right.right = new Node(10);
+		head.right.right.parent = head.right;
+
+		Node test = head.left.left;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.left.left.right;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.left;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.left.right;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.left.right.right;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.right.left.left;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.right.left;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		test = head.right;
+		System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+		// 10's next is null
+		test = head.right.right;
+		System.out.println(test.value + " next: " + getSuccessorNode(test));
+	}
+
+}
