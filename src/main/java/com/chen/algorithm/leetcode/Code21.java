207c1aa724b386b6be9df2862d0d5c4650de195c
chenzihan <chenzihan@thunisoft.com>
2022-09-13 11:22:50 +0800

    feat: 链表问题：合并两个有序链表

---
+++ src/main/java/com/chen/algorithm/leetcode/Code21.java
@@ -0,0 +1,62 @@
+package com.chen.algorithm.leetcode;
+
+/**
+ * code21 leetcode 第21题
+ * <a href="https://leetcode.cn/problems/merge-two-sorted-lists/submissions/">测试链接</a>
+ *
+ * @author chenzihan
+ * @date 2022/09/13
+ */
+public class Code21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode head;
+        if (list1.val > list2.val) {
+            head = list2;
+            list2 = list2.next;
+        } else {
+            head = list1;
+            list1 = list1.next;
+        }
+        ListNode cur = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                cur.next = list2;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        }
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
