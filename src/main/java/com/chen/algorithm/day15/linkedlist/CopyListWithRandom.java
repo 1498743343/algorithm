3fc828dab2d7c139a8e0958535c9cb4d8f98e863
chenzihan <chenzihan@thunisoft.com>
2022-09-22 16:21:39 +0800

    feat: 优化：复制带随机指针的链表

--- src/main/java/com/chen/algorithm/day15/linkedlist/CopyListWithRandom.java
+++ src/main/java/com/chen/algorithm/day15/linkedlist/CopyListWithRandom.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 给定一个由Node节点类型组成的无环单链表的头节点 head，请实现一个函数完成这个链表的复制，并返回复制的新链表的头节点
- * 测试链接 : https://leetcode.com/problems/copy-list-with-random-pointer/
+ * 复制带随机指针的链表
+ * <a href="https://leetcode.cn/problems/copy-list-with-random-pointer/">测试链接</a>
  *
  * @author chenzihan
  * @date 2022/07/04
@@ -24,6 +24,12 @@ public class CopyListWithRandom {
         }
     }
 
+    /**
+     * 使用哈希表缓存复制
+     *
+     * @param head 头
+     * @return {@link Node}
+     */
     public static Node copyRandomList1(Node head) {
         if (head == null) {
             return null;
@@ -44,38 +50,51 @@ public class CopyListWithRandom {
         return map.get(head);
     }
 
+    /**
+     * 空间复杂度 O(1)
+     * 整体分为三个步骤：
+     * 1.在原有链表的基础上进行复制，每个节点都复制一个节点，然后挂在自己的后面：原来：1 -> 2 -> 3，现在 1 -> 1 -> 2 -> 2 -> 3 -> 3
+     * 2.遍历 1 中建立好的链表，把随机指针放置好
+     * 3.拆分原有链表和新链表
+     *
+     * @param head 头
+     * @return {@link Node}
+     */
     public static Node copyRandomList2(Node head) {
         if (head == null) {
             return null;
         }
-        Node current = head;
+        // 第一步：复制链表，复制节点挂在原节点后面
+        Node cur = head;
         Node next;
-        while (current != null) {
-            next = current.next;
-            current.next = new Node(current.val);
-            current.next.next = next;
-            current = next;
+        while (cur != null) {
+            next = cur.next;
+            Node node = new Node(cur.val);
+            cur.next = node;
+            node.next = next;
+            cur = next;
         }
-        current = head;
-        while (current != null) {
-            Node newHead = current.next;
-            if (current.random != null) {
-                newHead.random = current.random.next;
-            } else {
-                newHead.random = null;
+        // 第二步：设置随即指针
+        cur = head;
+        Node random;
+        while (cur != null) {
+            random = cur.random;
+            if (random != null) {
+                cur.next.random = random.next;
             }
-            current = current.next.next;
+            cur = cur.next.next;
         }
-        Node result = head.next;
-        Node newCurrent;
-        current = head;
-        while (current != null) {
-            next = current.next.next;
-            newCurrent = current.next;
-            current.next = next;
-            newCurrent.next = next == null ? null : next.next;
-            current = next;
+        // 第三步：拆分
+        Node newHead = head.next;
+        cur = head;
+        Node newCur;
+        while (cur != null) {
+            next = cur.next.next;
+            newCur = cur.next;
+            newCur.next = next == null ? null : next.next;
+            cur.next = next;
+            cur = next;
         }
-        return result;
+        return newHead;
     }
 }
